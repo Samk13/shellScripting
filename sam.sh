4e53a78c99141c44b47b581219c37beb4491ec14
Samk13 <samarbid13@gmail.com>
2021-09-25 22:17:38 +0200

    first commit

---
+++ sam.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/bash
+# Echo somthing
+echo "Hello World"
+
+# # Variables shoudl be uppercase and start with a letter
+# # Variables can contain letters, numbers, underscores
+# NAME="Sam"
+# echo "My name is $NAME"
+
+# # User input
+read -p "Enter your name: " NAME
+echo "Hello $NAME"
+
+# # Simple if statement
+# if [ "$NAME" == "Sam" ]
+# then
+#     echo "Your name is Sam"
+# fi
+
+# If else statement
+# if [ "$NAME" == "Sam" ]
+# then
+#     echo "Your name is $NAME"
+# else
+#     echo "Your name is not Sam"
+# fi
+
+# comapre numbers
+# if [ "$NAME" -gt "10" ]
+# then
+#     echo "Your name is greater than 10"
+# else
+#     echo "Your name is less than 10"
+# fi
+
+# if values is equal -> -eq
+# if values is not equal -> -ne
+# if value is less than -> -lt
+# if value is less than or equal to -> -le
+# if value is greater than -> -gt
+# if value is greater than or equal to -> -ge
+
